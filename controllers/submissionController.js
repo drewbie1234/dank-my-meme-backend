651d2cc04f470366814fe3e84ec4d5deaa12f586
drewbie1234 <135838998+drewbie1234@users.noreply.github.com>
2024-05-20 00:42:59 +0100

    Updating routes, submission and votes page implemented, consolidated contest feed component for reusuability

--- controllers/submissionController.js
+++ controllers/submissionController.js
@@ -2,33 +2,15 @@ const Submission = require('../models/Submission');
 const Contest = require('../models/Contest');
 const mongoose = require('mongoose');
 
-/**
- * Create a new submission.
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
 const createSubmission = async (req, res) => {
     const { contest, userAddress, ipfsHash } = req.body;
     try {
-        // Log the request body
-        console.log("Creating submission with data:", JSON.stringify(req.body, null, 2));
-
         if (!ipfsHash) {
-            console.error("No IPFS hash provided.");
             return res.status(400).send("No IPFS hash provided");
         }
-
         const newSubmission = new Submission({ contest, wallet: userAddress, image: ipfsHash });
         await newSubmission.save();
-
-        // Update the contest with the new submission
-        const updatedContest = await Contest.findByIdAndUpdate(
-            contest, 
-            { $push: { submissions: newSubmission._id } }, 
-            { new: true }
-        );
-
-        console.log("New submission created:", newSubmission);
+        const updatedContest = await Contest.findByIdAndUpdate(contest, { $push: { submissions: newSubmission._id } }, { new: true });
         res.json(newSubmission);
     } catch (error) {
         console.error("Error submitting to contest:", error);
@@ -36,33 +18,17 @@ const createSubmission = async (req, res) => {
     }
 };
 
-/**
- * Get submissions by their IDs.
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
 const getSubmissionsByIds = async (req, res) => {
     const { submissionIds } = req.query;
-
-    // Log the request query parameters
-    console.log("Fetching submissions with IDs:", submissionIds);
-
     if (!submissionIds) {
-        console.error("No submission IDs provided.");
         return res.status(400).send("No submission IDs provided");
     }
-
     const ids = submissionIds.split(',').map(id => new mongoose.Types.ObjectId(id));
-
     try {
         const submissions = await Submission.find({ _id: { $in: ids } });
-
         if (submissions.length === 0) {
-            console.error("Submissions not found.");
             return res.status(404).send("Submissions not found");
         }
-
-        console.log("Fetched submissions:", submissions);
         res.json(submissions);
     } catch (error) {
         console.error("Error fetching submissions:", error);
@@ -70,43 +36,42 @@ const getSubmissionsByIds = async (req, res) => {
     }
 };
 
-/**
- * Get a submission by its ID.
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object.
- */
 const getSubmissionById = async (req, res) => {
     const { submissionId } = req.params;
-
-    // Log the request params
-    console.log("Fetching submission with ID:", submissionId);
-
     try {
         if (!mongoose.Types.ObjectId.isValid(submissionId)) {
-            console.error("Invalid submission ID.");
             return res.status(400).json({ message: 'Invalid submission ID' });
         }
-
         const submission = await Submission.findById(submissionId).populate('contest');
-
         if (!submission) {
-            console.error("Submission not found.");
             return res.status(404).json({ message: 'Submission not found' });
         }
-
         const contest = await Contest.findById(submission.contest._id).populate('submissions');
-
         if (!contest) {
-            console.error("Contest not found.");
             return res.status(404).json({ message: 'Contest not found' });
         }
-
         const response = { ...contest.toObject(), submissions: [submissionId] };
-
-        console.log("Fetched submission and associated contest:", response);
         res.json(response);
     } catch (error) {
         console.error('Failed to fetch submission and contest:', error);
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+const getContestBySubmissionId = async (req, res) => {
+    const { submissionId } = req.params;
+    try {
+        const submission = await Submission.findById(submissionId).populate('contest');
+        if (!submission) {
+            return res.status(404).send('Submission not found');
+        }
+        const contest = await Contest.findById(submission.contest._id).populate('submissions');
+        if (!contest) {
+            return res.status(404).send('Contest not found');
+        }
+        const response = { ...contest.toObject(), submissions: [submissionId] };
+        res.json(response);
+    } catch (error) {
+        console.error('Failed to fetch contest by submission:', error);
         res.status(500).json({ message: 'Server error' });
     }
 };
@@ -114,5 +79,6 @@ const getSubmissionById = async (req, res) => {
 module.exports = {
     createSubmission,
     getSubmissionsByIds,
-    getSubmissionById
+    getSubmissionById,
+    getContestBySubmissionId
 };
