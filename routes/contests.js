7b333d3cc96ed1e03fe22c66ea632a432dff57f0
drewbie1234 <135838998+drewbie1234@users.noreply.github.com>
2024-05-20 14:58:50 +0100

    Updating routes, submission and votes page implemented, consolidated contest feed component for reusuability

--- routes/contests.js
+++ routes/contests.js
@@ -11,21 +11,8 @@ const {
 } = require('../controllers/contestController');
 
 router.get('/', getContests);
+router.get('/:contestId', getContestById); // This route handles fetching contest by ID
 
-// Route to fetch contest by ID and render EJS template
-router.get('/:contestId', async (req, res) => {
-    const { contestId } = req.params;
-    try {
-        const contest = await getContestById(contestId);
-        if (!contest) {
-            return res.status(404).send('Contest not found');
-        }
-        res.render('contest', { contest });
-    } catch (error) {
-        console.error("Error fetching contest:", error);
-        res.status(500).send("Error fetching contest");
-    }
-});
 router.post('/', createContest);
 router.patch('/:contestId/end', endContest);
 router.patch('/:contestId/owner', updateContestOwner);
